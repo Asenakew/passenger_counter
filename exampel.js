11e9796c9774043e27700b33f599a24112aeea1c
Asenakew Zegeye <asenakew.zegeye@medieinstitutet.se>
2022-11-29 19:00:10 +0100

    learing new things

---
+++ exampel.js
@@ -0,0 +1,16 @@
+// calculate the human age with dog years
+//let myAge = 54;
+
+//let myAgeInDogYears = myAge * 7;
+//console.log(myAgeInDogYears);
+
+
+// scoring the score of a game
+let bounsPoints = 50;
+console.log(bounsPoints);
+bounsPoints = bounsPoints + 100;
+console.log(bounsPoints);
+bounsPoints = bounsPoints - 25;
+console.log(bounsPoints);
+bounsPoints = bounsPoints + 75;
+console.log(bounsPoints);
--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-hello world
+//document.getElementById("count-el").innreText = 5;
+//initianlize a count as 0
+// listen for click in the increment button
+// change the count-el in html to reflect  the new count
+function increment(){
+    console.log("the button is clicked");
+}
+increment()
+increment()
+
+
