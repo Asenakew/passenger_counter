309c25b7e981bda715f2e0a9021e8da40df9f5d4
Asenakew Zegeye <asenakew.zegeye@medieinstitutet.se>
2022-12-01 15:25:33 +0100

    added all the codes needed the app to work

--- script.js
+++ script.js
@@ -2,10 +2,24 @@
 //initianlize a count as 0
 // listen for click in the increment button
 // change the count-el in html to reflect  the new count
+let count = 0;
+let saveEl = document.getElementById("save-el");  
+let countEl = document.getElementById("count-el");
+
 function increment(){
-    console.log("the button is clicked");
+    count += 1;
+    countEl.textContent = count;
+   
+   
+    
 }
-increment()
-increment()
-
-
+function save(){
+   let countStr = count + "-";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+    countEl.textContent.object.style.overflow ="hidden";
+   
+  
+}
+save();
